d0bab37c03b39df8c5b07dd530d411e8e0df37d4
cyberyugun <yudigunawan52@gmail.com>
2020-02-21 11:14:54 +0700

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent  {
+export class HelloComponent  implements OnInit{
+  name='yudi'
    message = [{data:'yudi',id:'123243543'},{data:'test',id:'371256'}];
+   ngOnInit() {
+     //this.message.push({data:'gdaj',id:'6493287'})
+   }
 }
